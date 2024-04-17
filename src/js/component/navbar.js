a4b70dc73eee2178f4bebbe11b062bd1c2a21802
GusCastle25 <157417128+GusCastle25@users.noreply.github.com>
2024-04-17 22:39:29 +0000

    first commit

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,16 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+    return (
+        <nav className="navbar bg-transparent p-2">
+            <Link to="/">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                   <img
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/2560px-Star_Wars_Logo.svg.png"
+                        width="90"
+                        height="54"
+                    />
+                </a>
+            </div>
+            </Link>
+            <span className="mainTitle text-white"> A long time ago in a galaxy far, far away....</span>
+            <div className="ml-auto">
+                <Link to="/demo">
+                    <button className="btn btn-primary btn-outline-success">Favorites</button>
+                </Link>
+            </div>
+        </nav>
+    );
 };
+
